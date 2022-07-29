881d075c20645649cfeab8f4cded24c065dac188
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-07-29 18:00:34 +0900

    feat: 🎸 日時をファイル名の先頭に追記するように実装した

--- app.ts
+++ app.ts
@@ -1,4 +1,3 @@
 import GyazoToGoogleDrive from './GyazoToGoogleDrive'
 
-// new GyazoToGoogleDrive().main()
-new GyazoToGoogleDrive().printList({ page: 1, per_page: 5 })
+new GyazoToGoogleDrive().main()
