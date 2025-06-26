b8bb3d2003685cd2fdc12d406c899f687a46af58
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-06-26 16:04:26 +0900

    feat: 🎸 実際の利用状況を考えて細かい点などをまとめて修正 (#318)

---
+++ rclone_to_google_photos.example.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# downloaded_images/ 配下のファイルを削除する
+cd downloaded_images/
+if [ $(ls -1 | wc -l) -gt 100 ]; then
+  # shellcheck disable=SC2012
+  ls -t | tail -n +101 | xargs rm
+fi
+cd ..
+
+rclone copy --progress downloaded_images --include "*.*" <YOUR_REMOTE_NAME>:upload
+
+exit 0
