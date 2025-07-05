ff1c4a497dd003a2ccfc9c257277bcfad5fce70b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-07-05 12:20:07 +0900

    feat: 🎸 "per_page" を 50 から 100 に上げた

--- GyazoToGoogleDrive.ts
+++ GyazoToGoogleDrive.ts
@@ -1,9 +1,12 @@
 // @ts-ignore
-import Gyazo from 'gyazo-api'
-import * as fs from 'fs'
-import url from 'url'
+
 import { format } from 'date-fns'
 import * as dotenv from 'dotenv'
+import * as fs from 'fs'
+// @ts-ignore
+import Gyazo from 'gyazo-api'
+import url from 'url'
+
 dotenv.config()
 
 const client = new Gyazo(process.env.GYAZO_ACCESS_TOKEN)
@@ -17,57 +20,68 @@ type DownloadedFile = {
 
 class GyazoToGoogleDrive {
   constructor() {
+    // TODO: 書く
   }
 
   main() {
-    client.list({ page: 1, per_page: 50 })
-    .then((res: any) => {
-      const downloadedFiles: DownloadedFile[] = []
+    client
+      .list({ page: 1, per_page: 100 })
+      .then((res: any) => {
+        const downloadedFiles: DownloadedFile[] = []
 
-      res.data.forEach((item: any) => {
-        downloadedFiles.push(
-          {
+        res.data.forEach((item: any) => {
+          downloadedFiles.push({
             itemUrl: item.url,
             imageId: item.image_id,
-            getListedAt: new Date()
+            getListedAt: new Date(),
+          })
+        })
+
+        return downloadedFiles
+      })
+      .then((downloadedFiles: DownloadedFile[]) => {
+        downloadedFiles.forEach((DownloadedFile: DownloadedFile) => {
+          const parsedUrl = url.parse(DownloadedFile.itemUrl)
+          const pathname = parsedUrl.pathname || ''
+          const filename = pathname.split('/').pop()
+          const downloadedDirectory = 'downloaded_images'
+
+          if (
+            this.sameIdFileExistsInDownloadedFileLocalPath(
+              DownloadedFile.imageId,
+              downloadedDirectory
+            )
+          ) {
+            console.log(`${DownloadedFile.imageId} already exists.`)
+
+            return
           }
-        )
+
+          const downloadedFilename = `${format(
+            DownloadedFile.getListedAt,
+            'yyyyMMdd_HHmmss'
+          )}_${filename}`
+          const downloadedFileLocalPath = `${downloadedDirectory}/${downloadedFilename}`
+          const downloadedFileRemotePath = DownloadedFile.itemUrl
+
+          const wgetCommand = `wget -c -O ${downloadedFileLocalPath} ${downloadedFileRemotePath}`
+
+          if (process.env.NOT_EXECUTE_WGET_COMMAND === 'true') {
+            console.log(wgetCommand)
+          } else {
+            execSync(wgetCommand)
+          }
+        })
       })
-
-      return downloadedFiles
-    })
-    .then((downloadedFiles: DownloadedFile[]) => {
-      downloadedFiles.forEach((DownloadedFile: DownloadedFile) => {
-        const parsedUrl = url.parse(DownloadedFile.itemUrl)
-        const pathname = parsedUrl.pathname || ''
-        const filename = pathname.split('/').pop()
-        const downloadedDirectory = 'downloaded_images'
-
-        if (this.sameIdFileExistsInDownloadedFileLocalPath(DownloadedFile.imageId, downloadedDirectory)) {
-          console.log(`${DownloadedFile.imageId} already exists.`)
-
-          return
-        }
-
-        const downloadedFilename = `${format(DownloadedFile.getListedAt, 'yyyyMMdd_HHmmss')}_${filename}`
-        const downloadedFileLocalPath = `${downloadedDirectory}/${downloadedFilename}`
-        const downloadedFileRemotePath = DownloadedFile.itemUrl
-
-        const wgetCommand = `wget -c -O ${downloadedFileLocalPath} ${downloadedFileRemotePath}`
-
-        if (process.env.NOT_EXECUTE_WGET_COMMAND === 'true') {
-          console.log(wgetCommand)
-        } else {
-          execSync(wgetCommand)
-        }
+      .catch((err: any) => {
+        console.error({ err })
       })
-    })
-    .catch((err: any) => {
-      console.error({ err })
-    });
   }
 
-  sameIdFileExistsInDownloadedFileLocalPath(imageId: string, downloadedDirectory: string) {
+  sameIdFileExistsInDownloadedFileLocalPath(
+    imageId: string,
+    downloadedDirectory: string
+  ) {
     const files = fs.readdirSync(downloadedDirectory)
 
     for (const file of files) {
@@ -79,15 +93,16 @@ class GyazoToGoogleDrive {
     return false
   }
 
-  printList ({ page, per_page }: { page: number, per_page: number }) {
-    client.list({ page, per_page })
-      .then((res: any) => {
-        console.log(res.data)
-      })
+  printList({ page, per_page }: { page: number; per_page: number }) {
+    client.list({ page, per_page }).then((res: any) => {
+      console.log(res.data)
+    })
   }
 
   clearDownloadedImages() {
-    execSync('rm downloaded_images/*.jpg & rm downloaded_images/*.png & rm downloaded_images/*.gif & rm downloaded_images/*.mp4')
+    execSync(
+      'rm downloaded_images/*.jpg & rm downloaded_images/*.png & rm downloaded_images/*.gif & rm downloaded_images/*.mp4'
+    )
   }
 
   getExtensionFromFilename(filename: string) {
